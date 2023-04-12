f7c6911763127ccc0c1c9c7d6b0eb1bff3972d76
Ana <haeberli.ana@gmail.com>
2023-04-12 22:05:27 +0200

    API key and url updated

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
-let apiKey = "cb2582e5509724792e81e7b1fd00a6be";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+let apiKey = " cb2582e5509724792e81e7b1fd00a6be";
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
+console.log(apiUrl);
