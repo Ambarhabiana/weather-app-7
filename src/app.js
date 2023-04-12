712a74b69f6c4be02799ac11d92da25a8db51a16
Ana <haeberli.ana@gmail.com>
2023-04-12 22:24:32 +0200

    updated java

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
 let apiKey = " cb2582e5509724792e81e7b1fd00a6be";
-let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}`;
-console.log(apiUrl);
+let apiUrl = `api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
