08e7566eac0ae856bac19f242d02c334a6b67e34
Ana <haeberli.ana@gmail.com>
2023-04-14 16:35:55 +0200

    updated variable city

--- src/app.js
+++ src/app.js
@@ -39,6 +39,7 @@ function displayTemperature(response) {
 }
 
 let apiKey = "cb2582e5509724792e81e7b1fd00a6be";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Paris";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
