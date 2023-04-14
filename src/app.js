b8d16d85ec282cca80dbf500a15362ea96489b04
Ana <haeberli.ana@gmail.com>
2023-04-14 16:11:48 +0200

    elements update

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   humidityElement.innerHTML = response.data.main.humidity;
   let windElement = document.querySelector("#wind");
-  windElement.innerHTML = response.data.wind.speed;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "cb2582e5509724792e81e7b1fd00a6be";
